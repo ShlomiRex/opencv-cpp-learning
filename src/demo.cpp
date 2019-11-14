12d593cd5624d4e6d6264f55a0e6b3e6f856ef64
Shlomi Desktop <shlomid@protonmail.com>
2019-11-14 22:16:57 +0200

    Added more stuff

--- src/demo.cpp
+++ src/demo.cpp
@@ -2,102 +2,104 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
+
 #include <stdio.h>
 #include <opencv2/imgproc.hpp>
-#include <iostream>
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 using namespace std;
 using namespace cv;
 
 void laplican() {
 
-    Mat mat1, mat2, mat3;
-    mat1 = imread("assets/lena.png", IMREAD_COLOR);
-    
-    cout << "Origional Mat properties" << endl;
-    cout << "Type: " << mat1.type() << endl;
-    cout << "Depth: " << mat1.depth() << endl;
-    cout << "Channels: " << mat1.channels() << endl << endl;
+	Mat mat1, mat2, mat3;
+	mat1 = imread("assets/lena.png", IMREAD_COLOR);
 
-    cvtColor(mat1, mat2, COLOR_BGR2GRAY);
+	cout << "Origional Mat properties" << endl;
+	cout << "Type: " << mat1.type() << endl;
+	cout << "Depth: " << mat1.depth() << endl;
+	cout << "Channels: " << mat1.channels() << endl << endl;
 
-    cout << "Gray Mat properties" << endl;
-    cout << "Type: " << mat2.type() << endl;
-    cout << "Depth: " << mat2.depth() << endl;
-    cout << "Channels: " << mat2.channels() << endl << endl;
+	cvtColor(mat1, mat2, COLOR_BGR2GRAY);
 
-    Laplacian(mat2, mat3, 0);
+	cout << "Gray Mat properties" << endl;
+	cout << "Type: " << mat2.type() << endl;
+	cout << "Depth: " << mat2.depth() << endl;
+	cout << "Channels: " << mat2.channels() << endl << endl;
 
-    cout << "Laplican Mat properties" << endl;
-    cout << "Type: " << mat2.type() << endl;
-    cout << "Depth: " << mat2.depth() << endl;
-    cout << "Channels: " << mat2.channels() << endl << endl;
+	Laplacian(mat2, mat3, 0);
 
-    imshow("Original", mat1);
-    imshow("Gray", mat2);
-    imshow("Laplican", mat3);
-    
-    waitKey();
-    destroyAllWindows();
+	cout << "Laplican Mat properties" << endl;
+	cout << "Type: " << mat2.type() << endl;
+	cout << "Depth: " << mat2.depth() << endl;
+	cout << "Channels: " << mat2.channels() << endl << endl;
+
+	imshow("Original", mat1);
+	imshow("Gray", mat2);
+	imshow("Laplican", mat3);
+
+	waitKey();
+	destroyAllWindows();
 }
 
 void blurring()
 {
-  /// Declare variables
-  Mat src, dst;
+	/// Declare variables
+	Mat src, dst;
 
-  Mat kernel;
-  Point anchor;
-  double delta;
-  int ddepth;
-  int kernel_size;
+	Mat kernel;
+	Point anchor;
+	double delta;
+	int ddepth;
+	int kernel_size;
 
-  int c;
+	int c;
 
-  /// Load an image
-  src = imread("assets/lena.png", IMREAD_COLOR);
+	/// Load an image
+	src = imread("assets/lena.png", IMREAD_COLOR);
 
-  /// Create window
-  //namedWindow( window_name, CV_WINDOW_AUTOSIZE );
+	/// Create window
+	//namedWindow( window_name, CV_WINDOW_AUTOSIZE );
 
-  /// Initialize arguments for the filter
-  anchor = Point( -1, -1 );
-  delta = 0;
-  ddepth = -1;
+	/// Initialize arguments for the filter
+	anchor = Point(-1, -1);
+	delta = 0;
+	ddepth = -1;
 
-  /// Loop - Will filter the image with different kernel sizes each 0.5 seconds
-  int ind = 0;
-  while( true )
-    {
-      c = waitKey(500);
-      /// Press 'ESC' to exit the program
-      if( (char)c == 27 )
-        { break; }
+	/// Loop - Will filter the image with different kernel sizes each 0.5 seconds
+	int ind = 0;
+	while (true)
+	{
+		c = waitKey(500);
+		/// Press 'ESC' to exit the program
+		if ((char)c == 27)
+		{
+			break;
+		}
 
-      /// Update kernel size for a normalized box filter
-      kernel_size = 3 + 2*( ind%5 );
-      kernel = Mat::ones( kernel_size, kernel_size, CV_32F )/ (float)(kernel_size*kernel_size);
+		/// Update kernel size for a normalized box filter
+		kernel_size = 3 + 2 * (ind % 5);
+		kernel = Mat::ones(kernel_size, kernel_size, CV_32F) / (float)(kernel_size * kernel_size);
 
-        cout << "Kernel size: " << kernel_size << endl;
+		cout << "Kernel size: " << kernel_size << endl;
 
-      /// Apply filter
-      filter2D(src, dst, ddepth , kernel, anchor, delta, BORDER_DEFAULT );
-      imshow( "filter2D Demo", dst );
-      ind++;
-    }
+		/// Apply filter
+		filter2D(src, dst, ddepth, kernel, anchor, delta, BORDER_DEFAULT);
+		imshow("filter2D Demo", dst);
+		ind++;
+	}
 
-    destroyAllWindows();
+	destroyAllWindows();
 }
 
 /*
 This function shows pixel manipulation with 1 channel and 3 channels
 */
 void rgb() {
-    Mat mat;
+	Mat mat;
 	mat = imread("assets/lena.png", IMREAD_COLOR);
 	vector<Mat> bgr_planes;
 	split(mat, bgr_planes);
@@ -105,7 +107,7 @@ void rgb() {
 	imshow("Blue", bgr_planes[0]);
 	imshow("Green", bgr_planes[1]);
 	imshow("Red", bgr_planes[2]);
-	
+
 	Mat mat1, mat2, mat3;
 	cvtColor(bgr_planes[0], mat1, COLOR_GRAY2BGR); //Blue
 	cvtColor(bgr_planes[1], mat2, COLOR_GRAY2BGR); //Green
@@ -114,8 +116,8 @@ void rgb() {
 	cout << "BGR 3 channels type:" << mat1.type() << endl;
 
 	//Because we converted GRAY to BGR, we have 3 channels of color instead of just 1. 
-	for(int i = 0; i < mat1.rows; i++) {
-		for(int j = 0; j < mat1.cols; j++) {
+	for (int i = 0; i < mat1.rows; i++) {
+		for (int j = 0; j < mat1.cols; j++) {
 			Vec3b& colour = mat1.at<Vec3b>(j, i);
 			//colour[0] = 0 //Blue
 			colour[1] = 0; //Green
@@ -123,8 +125,8 @@ void rgb() {
 		}
 	}
 
-	for(int i = 0; i < mat2.rows; i++) {
-		for(int j = 0; j < mat2.cols; j++) {
+	for (int i = 0; i < mat2.rows; i++) {
+		for (int j = 0; j < mat2.cols; j++) {
 			Vec3b& colour = mat2.at<Vec3b>(j, i);
 			colour[0] = 0; //Blue
 			//colour[1] = 0; //Green
@@ -132,21 +134,21 @@ void rgb() {
 		}
 	}
 
-	for(int i = 0; i < mat3.rows; i++) {
-		for(int j = 0; j < mat3.cols; j++) {
+	for (int i = 0; i < mat3.rows; i++) {
+		for (int j = 0; j < mat3.cols; j++) {
 			Vec3b& colour = mat3.at<Vec3b>(j, i);
 			colour[0] = 0; //Blue
 			colour[1] = 0; //Green
 			//colour[2] = 0; //Red
 		}
 	}
-	
+
 	imshow("Blue 2", mat1);
 	imshow("Green 2", mat2);
 	imshow("Red 2", mat3);
 
 	waitKey();
-    destroyAllWindows();
+	destroyAllWindows();
 }
 
 /*
@@ -191,9 +193,9 @@ void color_mask() {
 	vector<Mat> bgr_planes;
 	split(mat, bgr_planes);
 
-	Mat blue = bgr_planes[0], green =  bgr_planes[1], red = bgr_planes[2];
+	Mat blue = bgr_planes[0], green = bgr_planes[1], red = bgr_planes[2];
 	imshow("Blue", blue);
-	imshow("Green",green);
+	imshow("Green", green);
 	imshow("Red", red);
 
 	Mat bin_mask_blue, output, output2;
@@ -204,8 +206,8 @@ void color_mask() {
 	imshow("Binary mask (blue) applied", output);
 
 	output.copyTo(output2);
-	for(int i = 0; i < output2.rows; i++) {
-		for(int j = 0; j < output2.cols; j++) {
+	for (int i = 0; i < output2.rows; i++) {
+		for (int j = 0; j < output2.cols; j++) {
 			Vec3b& colour = output2.at<Vec3b>(j, i);
 			//Image is BGR, so, change only channel #1 and #2 leave #0 as is
 			colour[1] = 0;
@@ -237,7 +239,7 @@ void image_color_histogram()
 	calcHist(&bgr_planes[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate);
 	int hist_w = 512, hist_h = 400;
 	int bin_w = cvRound((double)hist_w / histSize);
-	Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
+	Mat histImage(hist_h, hist_w,  CV_8UC3, Scalar(0, 0, 0));
 	normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
 	normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
 	normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat());
@@ -260,7 +262,7 @@ void image_color_histogram()
 
 void adaptive_threshold() {
 	Mat mat, out, out2, out3;
-    mat = imread("assets/lena.png", IMREAD_COLOR);
+	mat = imread("assets/lena.png", IMREAD_COLOR);
 	cvtColor(mat, mat, COLOR_BGR2GRAY);
 
 	threshold(mat, out3, 127, 255, THRESH_BINARY);
@@ -278,7 +280,7 @@ void adaptive_threshold() {
 
 void otsu_threshold() {
 	Mat mat, gray, out, out2, out3, out4, out5;
-    mat = imread("assets/lena.png", IMREAD_COLOR);
+	mat = imread("assets/lena.png", IMREAD_COLOR);
 	cvtColor(mat, gray, COLOR_BGR2GRAY);
 
 	//The value 127 is not used! Otsu algorithem finds optimal value for thresholding (middle of histogram peaks is optimal)
@@ -292,13 +294,13 @@ void otsu_threshold() {
 
 	imshow("Otsu threshold", out2);
 
-	Size ksize = {5,5};
+	Size ksize = { 5,5 };
 	GaussianBlur(gray, out3, ksize, 0);
 	//Notice the sum of THRESH types
 	threshold(out3, out4, 0, 255, THRESH_OTSU + THRESH_BINARY);
 	imshow("Gaussian threshold", out4);
 	mat.copyTo(out5, out4);
-	imshow("Otsu + Binary + Gaussian",out5);
+	imshow("Otsu + Binary + Gaussian", out5);
 
 
 	waitKey();
@@ -307,7 +309,7 @@ void otsu_threshold() {
 
 void denoising() {
 	Mat mat, gray, out, out2, out3, out4, out5;
-    mat = imread("assets/lena.png", IMREAD_COLOR);
+	mat = imread("assets/lena.png", IMREAD_COLOR);
 	cvtColor(mat, gray, COLOR_BGR2GRAY);
 	imshow("Original", mat);
 
@@ -325,7 +327,7 @@ void denoising() {
 	while (true)
 	{
 		//Change the brightness and contrast of the image (For more infomation http://opencv-srf.blogspot.com/2013/07/change-contrast-of-image-or-video.html)
-		
+
 		//mat.convertTo(dst, -1, 1, iSliderValue); 
 		fastNlMeansDenoisingColored(mat, dst, iSliderValue);
 
@@ -348,7 +350,7 @@ void denoising() {
 
 void sobel() {
 	Mat mat, gray, out, out2, out3, out4, out5;
-    mat = imread("assets/lena.png", IMREAD_COLOR);
+	mat = imread("assets/lena.png", IMREAD_COLOR);
 	cvtColor(mat, gray, COLOR_BGR2GRAY);
 	imshow("Original", mat);
 
@@ -368,34 +370,60 @@ void sobel() {
 	destroyAllWindows();
 }
 
+void hsv_color_picker() {
+	Mat mat1, mat2, mat3, mat4;
+	namedWindow("Color");
+
+	int saturation = 0, hue = 0, value = 0;
+
+	createTrackbar("Hue", "Color", &hue, 179);
+	createTrackbar("Saturation", "Color", &saturation, 255);
+	createTrackbar("Value", "Color", &value, 255);
+
+	while (true) {
+		mat1 = Mat(800, 600, CV_8UC3);
+		mat1.convertTo(mat1, COLOR_BGR2HSV);
+		mat1.setTo(Scalar(hue, saturation, value));
+		mat1.convertTo(mat1, COLOR_HSV2BGR);
+		imshow("Color", mat1);
+		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
+	destroyAllWindows();
+}
+
 /*
 Captures video from camera, and thresholds the RED color of the video.
 The good pixels are displayed in white while the bad pixels (which do not pass the thresholding) are black
 Original code: https://www.opencv-srf.com/2010/09/object-detection-using-color-seperation.html
 I added to the code the original image, after the mask applied (threshold mask) so we can see color instead of black and white elipse
 */
-void red_color_threshold() {
+void hsv_threshold() {
 	VideoCapture cap(0); //capture the video from web cam
+	
 
-    if ( !cap.isOpened() )  // if not success, exit program
-    {
+	if (!cap.isOpened())  // if not success, exit program
+	{
 		cout << "Cannot open the web cam" << endl;
-        exit(1);
-    }
+		exit(1);
+	}
 
-    namedWindow("Control"); //create a window called "Control"
+	namedWindow("Control"); //create a window called "Control"
 
 	int iLowH = 0;
 	int iHighH = 179;
 
-	int iLowS = 0; 
+	int iLowS = 0;
 	int iHighS = 255;
 
 	int iLowV = 0;
 	int iHighV = 255;
 
 	//Create trackbars in "Control" window
-	
+
 	createTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
 	createTrackbar("HighH", "Control", &iHighH, 179);
 
@@ -405,33 +433,33 @@ void red_color_threshold() {
 	createTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
 	createTrackbar("HighV", "Control", &iHighV, 255);
 
-    while (true)
-    {
-        Mat imgOriginal;
+	while (true)
+	{
+		Mat imgOriginal;
 
-        bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+		bool bSuccess = cap.read(imgOriginal); // read a new frame from video
 
-         if (!bSuccess) //if not success, break loop
-        {
-             cout << "Cannot read a frame from video stream" << endl;
-             break;
-        }
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
 
 		Mat imgHSV;
 
 		cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
-		
+
 		Mat imgThresholded;
 
 		inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
-			
+
 		//morphological opening (remove small objects from the foreground)
-		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
-		dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
 		//morphological closing (fill small holes in the foreground)
-		dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
-		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+		dilate(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+		erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
 
 		imshow("Thresholded Image - Mask", imgThresholded); //show the thresholded image
 		imshow("Original", imgOriginal); //show the original image
@@ -441,12 +469,45 @@ void red_color_threshold() {
 
 		imshow("Thresholded Image", imgAfterMask); //I added this to the code to give it a little touch
 
-		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+		if (waitKey(30) == 27)
 		{
 			cout << "esc key is pressed by user" << endl;
-			break; 
+			break;
 		}
-    }
+	}
+	destroyAllWindows();
+}
+
+void canny_edge_detection() {
+	VideoCapture cap(0); //capture the video from web cam
+	namedWindow("Control");
+
+	int threshold1 = 10, threshold2 = 90;
+	createTrackbar("Threshold1", "Control", &threshold1, 300);
+	createTrackbar("Threshold2", "Control", &threshold2, 300);
+	while(true) {
+		Mat imgOriginal;
+		Mat imgCanny;
+
+		bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+
+		if (!bSuccess) //if not success, break loop
+		{
+			cout << "Cannot read a frame from video stream" << endl;
+			break;
+		}
+
+		Canny(imgGray, imgCanny, threshold1, threshold2);
+		imshow("Canny", imgCanny);
+
+		if (waitKey(1) == 27)
+		{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
+
+	destroyAllWindows();
 }
 
 int main()
@@ -455,13 +516,17 @@ int main()
 	//image_color_histogram();
 	//binary_trunc_mask();
 	//color_mask();
-    //rgb();
-    //blurring();
+	//rgb();
+	//blurring();
 	//otsu_threshold();
-    //laplican();
+	//laplican();
 	//adaptive_threshold();
 	//denoising();
+	//hsv_color_picker();        //TODO: Fix
+	//hsv_threshold();
+	canny_edge_detection();
 
-	red_color_threshold();
-    return 0;
+
+
+	return 0;
 }
